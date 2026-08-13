61879b12653156777ded75b746e262c26c62bad9
juliet <julietmeletta@gmail.com>
2026-08-12 23:30:40 -0400

    did calculations for map

--- jsFiles/map.js
+++ jsFiles/map.js
@@ -1,4 +1,4 @@
-const date = new Date(Date.UTC(2000, 0, 1, 12, 0, 0));
+let date = new Date(Date.UTC(2000, 0, 1, 12, 0, 0));
 
 function timeToGMST(date) {
     let year = date.getUTCFullYear();
@@ -35,5 +35,41 @@ function LSTtoH(LST, RA) {
   return H;
 }
 
+function degToRad(deg) {return deg * (Math.PI / 180);}
+function radToDeg(rad) {return rad * (180 / Math.PI);}
+
+function calcAltitude(h, dec, latitude) {
+    let H = degToRad(h);
+    let Dec = degToRad(dec);
+    let la = degToRad(latitude);
+    let altitude = Math.sin(Dec)*Math.sin(la)+Math.cos(Dec)*Math.cos(la)*Math.cos(H);
+    altitude = Math.max(-1, Math.min(1, altitude));
+    altitude = Math.asin(altitude);
+    altitude = radToDeg(altitude);
+    return altitude;
+}
+
+function calcAzimuth(h, dec, latitude) {
+    let H = degToRad(h);
+    let Dec = degToRad(dec);
+    let la = degToRad(latitude);
+    let y = -Math.sin(H)*Math.cos(Dec);
+    let x = Math.sin(Dec)*Math.cos(la)-Math.cos(Dec)*Math.cos(H)*Math.sin(la);
+    let azimuth = Math.atan2(y,x);
+    azimuth = radToDeg(azimuth);
+    azimuth = ((azimuth % 360) + 360) % 360;
+    return azimuth;
+}
+
+function calcPos(latitude, longitude, time, ra, dec) {
+    let gmst = timeToGMST(time);
+    let lst = GMSTtoLST(gmst, longitude);
+    let h = LSTtoH(lst, ra);
+    let altitude = calcAltitude(h, dec, latitude);
+    let azimuth = calcAzimuth(h, dec, latitude);
+    const data = {altitude, azimuth};
+    return data;
+}
+
 const c = document.getElementById("calc");
-c.innerHTML = GMST;
+c.innerHTML = JSON.stringify(calcPos(42.365589, -71.010025, new Date(Date.UTC(2026, 7, 13, 3, 5, 0, 0)), 284.0592503, 44.5183733));
